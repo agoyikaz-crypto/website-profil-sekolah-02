e5b2a83a85a8807015f152d20d0a005cadfea7e2
agoyikaz-crypto <agoyikaz@gmail.com>
2026-05-05 16:03:08 +0700

    redesain global-01

---
+++ js/animations.js
@@ -0,0 +1,22 @@
+// LENIS SMOOTH SCROLL
+const lenis = new Lenis();
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+// GSAP ANIMATIONS
+gsap.utils.toArray(".section, .card, .feature-card, .news-card").forEach((el) => {
+  gsap.from(el, {
+    y: 40,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: el,
+      start: "top 85%",
+    },
+  });
+});
